3ae2dda61dd1421f2923944e09f4050c267409e4
shad mustafa <shadmanmi2018@gmail.com>
2022-10-23 22:18:58 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
+  if (a <= 400 ){
+return 'This one is on me!';
+  }
+  else if(a > 400 && a<2000){
+return 'That will be twenty bucks.';
+  }
+  else if(a> 2000 ){
+  
+    if(a > 2500){
+      return 'No can do.';
+        }
+        else
+        return 'I will gladly take your thirty bucks.';
+
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city==='NYC'){
+    return "Ok, sounds good.";
+  }
+  else
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':{ return "Thank you so much."};
+    case 'not as generous':{ return 'Thank you.'};
+    default: { return 'Bye.'};
+  }
+
+
 }
